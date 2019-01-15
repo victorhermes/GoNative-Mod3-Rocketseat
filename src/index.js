d298a783980af69eb615092eaf2c2dc20606c77c
Victor Hermes <me.victorhermes@gmail.com>
2019-01-15 19:04:52 -0200

    Configurando novo app e atts

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import React, { Component } from "react";
 import { View, Text } from "react-native";
 import { Provider } from "react-redux";
 import store from "~/store";
-import TodoList from "./TodoList";
 
 export default class App extends Component {
     render() {
         return (
             <Provider store={store}>
-                <TodoList />
+                <View>
+                    <Text>New</Text>
+                </View>
             </Provider>
         );
     }
