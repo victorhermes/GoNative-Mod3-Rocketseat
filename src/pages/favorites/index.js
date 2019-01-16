493cfe61beddb2b3c7872ff06052f9cd84bd580f
Victor Hermes <me.victorhermes@gmail.com>
2019-01-16 20:27:44 -0200

    Navegação

---
+++ src/pages/favorites/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const Favorites = () => <View />;
+
+export default Favorites;
--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -8,45 +8,54 @@ import {
     SafeAreaView
 } from "react-native";
 
-const Main = () => (
-    <SafeAreaView style={styles.container}>
-        <View style={styles.content}>
-            <Text style={styles.title}>Gitmark</Text>
-            <Text style={styles.description}>
-                Começe adicionando alguns repositórios aos seus favoritos
-            </Text>
+export default class Main extends Component {
+    static navigationOptions = {
+        header: null
+    };
 
-            <View style={styles.form}>
-                <TextInput
-                    style={styles.input}
-                    autoCapitalize="none"
-                    autoCorrect={false}
-                    placeholder="usuário/repositório"
-                    underlineColorAndroid="transparent"
-                />
+    navigationToFavorites = () => {
+        this.props.navigation.navigate("Favorites");
+    };
 
-                <TouchableOpacity
-                    style={styles.button}
-                    onPress={() => {}}
-                    activeOpacity={0.6}
-                >
-                    <Text style={styles.buttonText}>
-                        Adicionar aos favoritos
+    render() {
+        return (
+            <SafeAreaView style={styles.container}>
+                <View style={styles.content}>
+                    <Text style={styles.title}>Gitmark</Text>
+                    <Text style={styles.description}>
+                        Começe adicionando alguns repositórios aos seus
+                        favoritos
                     </Text>
-                </TouchableOpacity>
-            </View>
-        </View>
 
-        <View style={styles.footer}>
-            <TouchableOpacity onPress={() => {}}>
-                <Text style={styles.footerLink}>Meus repositórios (3)</Text>
-            </TouchableOpacity>
-        </View>
-    </SafeAreaView>
-);
+                    <View style={styles.form}>
+                        <TextInput
+                            style={styles.input}
+                            autoCapitalize="none"
+                            autoCorrect={false}
+                            placeholder="usuário/repositório"
+                            underlineColorAndroid="transparent"
+                        />
 
-Main.navigationOptions = {
-    header: null
-};
+                        <TouchableOpacity
+                            style={styles.button}
+                            onPress={() => {}}
+                            activeOpacity={0.6}
+                        >
+                            <Text style={styles.buttonText}>
+                                Adicionar aos favoritos
+                            </Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
 
-export default Main;
+                <View style={styles.footer}>
+                    <TouchableOpacity onPress={this.navigationToFavorites}>
+                        <Text style={styles.footerLink}>
+                            Meus repositórios (3)
+                        </Text>
+                    </TouchableOpacity>
+                </View>
+            </SafeAreaView>
+        );
+    }
+}
